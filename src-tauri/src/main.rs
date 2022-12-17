88d35d7cdb11c2a1acbfeb60a4622d1779d5b2d4
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-12-17 12:07:21 -0300

    Ajustes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,27 @@ fn greet(name: &str) -> String {
     format!("So {} you re trying to call RUST from Typescript", name)
 }
 
+#[tauri::command]
+fn get_documents(conn: &mut my::Conn, filter: &str) -> Result<Vec<Document>, Box<dyn Error>> {
+    let mut documents = Vec::new();
+    let query = if filter.is_empty() {
+        "SELECT * FROM `commercedb`.`produto`".to_string()
+    } else {
+        format!("SELECT * FROM `commercedb`.`produto` WHERE {}", filter)
+    };
+    let rows = conn.query(query)?;
+    for row in rows {
+        let (nome, foto, formato_imagem) = my::from_row(row?);
+        let formato_imagem = get_file_format(&formato_imagem);
+        documents.push(Document {
+            nome,
+            foto,
+            formato_imagem,
+        });
+    }
+    Ok(documents)
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
